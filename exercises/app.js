fb6bc47774fc4d97665bffa38304533b8a1f9eca
CAUÊ RODRIGUES DOS REIS <caue.reis@senaisp.edu.br>
2026-02-04 13:26:39 -0300

    feat: exercise 1 completed

---
+++ exercises/app.js
@@ -0,0 +1,10 @@
+let nome = 'Cauê'
+let idade = 19
+let numeroUm = 7
+let numeroDois = 3
+
+function exibirDados() {
+    console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e a soma entre ${numeroUm} e ${numeroDois} é igual a ${numeroUm + numeroDois}.`)
+}
+
+exibirDados()
